3f42877d96d9bff06418879b047f0b2f687d959a
HarshDBugger <mac@Macs-MacBook-Air.local>
2025-04-24 17:43:28 +0530

    Pew Pew

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def removeitem():
 
 port = int(os.environ.get('PORT', 5000))
 
-app.run(debug=True,host='0.0.0.0',
-        debug=True,
+app.run(debug=True,
+        host='0.0.0.0',
         use_reloader=False )
 #   
