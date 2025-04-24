bc9507575b738b5b1691c20137b704d9ae7e10f7
HarshDBugger <mac@Macs-MacBook-Air.local>
2025-04-24 18:11:39 +0530

    Cors Updated

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['JWT_SECRET_KEY'] = 'SwarnAbhusan'
 db.init_app(app)
-CORS(app,origins=['http://localhost:5173'],supports_credentials=True,max_age=10800)
+CORS(app,origins=['https://swarnabhusan.netlify.app'],supports_credentials=True,max_age=10800)
 
 jwt=JWTManager(app)
 
